5fb367a2d9c7d534257e4452f3514bfd63fde108
Bilawal Ali Faiz <bilawalalifaiz.dev@gmail.com>
2025-10-23 21:57:59 +0500

    Array programs

---
+++ chapter 07/01 input output array/10 a program that initilize array .It input a value to the user and searches the number in array.cpp
@@ -0,0 +1,17 @@
+//write a program that initialize an array.it inputs a value form the user and searches the number in an array .
+#include<iostream>;
+using namespace std;
+int main()
+{
+	int arr[10]{ 10,20,30,40,50,60,70,80,90,100};
+	int i,n,loc=-1;
+	cout<<"Enter value to find :";
+	cin>>n;
+	for(i=0;i<10;i++)
+	if(arr[i]==n)
+	loc=i;
+	if(loc==-1)
+	cout<<"Value not found in array :";
+	else
+	cout<<"value found in array :"<<loc;
+}
